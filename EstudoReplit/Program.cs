c9678014bd3122394b3e46253b726ef99e275dbf
Victor Hugo Caldas Araujo <victor.hugo@localiza.com>
2022-03-29 15:53:55 -0300

    Add project files.

---
+++ EstudoReplit/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EstudoReplit.Entidade;
+
+Console.WriteLine("Hello, World!");
+var carro = new Carro();
+carro.Nome = "Gol";
+Console.WriteLine(carro.Nome);
